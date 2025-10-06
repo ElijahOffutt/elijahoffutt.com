037fe29d8d3a541f283ea2aa7209de3b1a2d0468
ElijahOffutt <Einzenheim@yahoo.com>
2025-10-06 08:14:16 -0400

    building the public pages and splash screen

---
+++ app/components/PublicFooter.vue
@@ -0,0 +1,7 @@
+<template>
+    <UFooter class="public-footer">
+        <div class="text-center">
+            <p>&copy; 2024 Collaborating Docs. All rights reserved.</p>
+        </div>
+    </UFooter>
+</template>
---
+++ app/components/PublicHeader.vue
@@ -0,0 +1,27 @@
+<template>
+    <u-header>
+
+        <template #left>
+
+            <NuxtLink to="/docs">
+                <!-- <Logo class="h-6 w-auto" /> -->
+                 <h1>Collaborating Docs</h1>
+              </NuxtLink>
+
+        </template>
+        
+        <template #right>
+
+            <NuxtLink to="/login" class="u-header-link">
+                Log in
+            </NuxtLink>
+            <NuxtLink to="/register" class="u-header-link u-header-link-primary">
+                Sign up
+            </NuxtLink>
+
+            <u-button>sample</u-button>
+
+        </template>
+
+    </u-header>
+</template>
