82cb9c7f508b5717b97b36f6c776f90741f0df99
ElijahOffutt <Einzenheim@yahoo.com>
2025-10-10 17:38:32 -0400

    updates

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,31 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   ssr: false,
-  nitro: {
-    preset: 'github_pages', // Use this when deploying to GitHub Pages
-  },
-  app: {
-    baseURL: 'https://elijahoffutt.com', // Change to your actual repo name
-  },
+
   modules: [
     '@nuxt/eslint',
     '@nuxt/ui',
     '@vueuse/nuxt',
   ],
 
-
   devtools: {
     enabled: true
   },
 
   css: ['~/assets/css/main.css'],
-
-  routeRules: {
-    '/api/**': {
-      cors: true
-    },
-    '/**': { prerender: false }
-  },
 
   compatibilityDate: '2024-07-11',
 
