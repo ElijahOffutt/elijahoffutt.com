ad3bda02f9113a65a51237bb57d3ca4615f840d6
ElijahOffutt <Einzenheim@yahoo.com>
2025-10-11 23:21:09 -0400

    centering video

---
+++ .nuxt/dist/server/client.manifest.mjs
@@ -0,0 +1,18 @@
+export default {
+  "@vite/client": {
+    "prefetch": true,
+    "isEntry": true,
+    "file": "@vite/client",
+    "css": [],
+    "module": true,
+    "resourceType": "script"
+  },
+  "D:/dev/elijahoffutt.com/node_modules/.pnpm/nuxt@4.1.2_@parcel+watcher@_711ded5c0b0a777b943ae0cc30596ebb/node_modules/nuxt/dist/app/entry.async.js": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "isEntry": true,
+    "file": "D:/dev/elijahoffutt.com/node_modules/.pnpm/nuxt@4.1.2_@parcel+watcher@_711ded5c0b0a777b943ae0cc30596ebb/node_modules/nuxt/dist/app/entry.async.js"
+  }
+}
